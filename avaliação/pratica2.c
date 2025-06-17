a042e27c81c8c8b4ea6895d7ba8757f204a8eb65
MaAz5002 <mvitoria5002@gmail.com>
2025-06-16 22:06:41 -0300

    pratica2.c

--- avaliação/pratica2.c
+++ avaliação/pratica2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_PRODUTOS 50
-   
+#define MAX_PRODUTOS 50 // armezena o numero total de produtos possiveis dentro do vetor
+
+//Declaração da estrutura
 struct Produto{
         int codigo;
         char nome[50];
@@ -9,19 +10,20 @@ struct Produto{
         int quantidade_estoque;
     };
 
-
+// procedimento exclusivo para cadastro dos produtos
 int cadastrar_produto(struct Produto lista_produto[], int *num_produto){
     int quantidade_produto;// armazena o numero de produtos que serão alocados
     int produto_cadastrados = 0; //  numero de produtos cadastrados dentro da lista
 
     printf("----- Cadastrar novos produtos ------\n");
     printf("Quantos produtos deseja cadastrar?\n");
-    scanf(" %d ", &quantidade_produto);// solicita a quantidade de produtos
+    scanf(" %d ", &quantidade_produto);// solicita a numero de produtos
 
     if(produto_cadastrados <= 0){
         printf("Insira um valor valido!\n");
     }
 
+    //validaçao para impedimento de cadastro de produtos além do definido
     if ((*num_produto + quantidade_produto) > MAX_PRODUTOS) {
         printf("Impossivel cadastrar %d produtos. O limite de %d sera excedido.\n",
                quantidade_produto, MAX_PRODUTOS);
@@ -37,36 +39,37 @@ int cadastrar_produto(struct Produto lista_produto[], int *num_produto){
 
 
 
-
+//laço de repetição para adicionar os produtos a lista
     for(int i = 0; i < quantidade_produto; i++){
         int num_indice = *num_produto + produto_cadastrados;
 
         printf("------ Dados do Produto ------");
         printf(" Código: ");
-        scanf(" %d", &lista_produto[num_indice].codigo);
+        scanf(" %d", &lista_produto[num_indice].codigo);// adiciona o codigo
 
-        printf(" Nome (sem espacos): "); // ALERTA: scanf("%s") para no primeiro espaço
-        scanf(" %s", lista_produto[num_indice].nome);
+        printf(" Nome (sem espacos): "); 
+        scanf(" %s", lista_produto[num_indice].nome);// adiciona o nome
 
         printf(" Preço: ");
-        scanf(" %f", &lista_produto[num_indice].preco);
+        scanf(" %f", &lista_produto[num_indice].preco);// adiciona o preço
 
         printf(" Quantidade em estoque: ");
-        scanf(" %d", &lista_produto[num_indice].quantidade_estoque);
+        scanf(" %d", &lista_produto[num_indice].quantidade_estoque);// adiciona o estoque do produto
 
-        produto_cadastrados++;
+        produto_cadastrados++; // incrmenta ao numero de produtos cadastrados
 
 
 
     }
 
-    *num_produto += produto_cadastrados;
+    *num_produto += produto_cadastrados;// o numero de produtos é atualizado 
     return produto_cadastrados;
 
 };
 
-
+//Procedimento para mostrar os dados dos produtos cadastrados
 void dadosProdutos( struct Produto lista_produto[], int num_produto){
+    // verificação do vetor
     if (num_produto == 0) {
         printf("Nenhum produto cadastrado atualmente.\n");
         return; 
@@ -85,19 +88,19 @@ void dadosProdutos( struct Produto lista_produto[], int num_produto){
 }
 
 
-
+// Função principal
 int main(){
-    int entrada_usuario;
+    int entrada_usuario; //armazena qual a funçao pelo usuario
     struct Produto estoque[MAX_PRODUTOS];
     int qtd_produto = 0;
     do
     {
         printf("Insira a operação desejada para continuar:\n\
-       1 - Cadastrar novo produto\n\
-       2 - Exibir dados do produto\n\
-       3 - Sair");
+        1 - Cadastrar novo produto\n\
+        2 - Exibir dados do produto\n\
+        3 - Sair");
     
-    scanf(" %d", &entrada_usuario);
+        scanf(" %d", &entrada_usuario);
    
     switch (entrada_usuario){
     
